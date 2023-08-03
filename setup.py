1707f52878d714ffb270384f4e7420d55225ced0
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-03 18:53:44 +0530

    Creating Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="santhosh kumar",
+    author_email="santhosh102002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
