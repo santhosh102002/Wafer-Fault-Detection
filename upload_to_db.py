68b7a5e309e219e3d988ac242a0e48f4b5ecf6a3
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-05 01:21:02 +0530

    upload_to_db.py is updated

--- upload_to_db.py
+++ upload_to_db.py
@@ -1,16 +1,15 @@
 from pymongo.mongo_client import MongoClient
 import json
 import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-url = "mongodb+srv://santhoshNode:santhoshNode@cluster0.ofceqbd.mongodb.net/"
+client = MongoClient(os.getenv("MONGO_URL"))
 
-client = MongoClient(url)
-
-DATABASE = "waferDB"
-COLLECTION = "WaferCollection"
 
 df = pd.read_csv(r"E:\WAFER_FAULT_DETECTION\notebooks\wafer_data.csv")
 df.drop(['Unnamed: 0'],axis=1,inplace=True)
 json_record = list(json.loads(df.T.to_json()).values())
 
-client[DATABASE][COLLECTION].insert_many(json_record)
+client[os.getenv("WAFER_DB")][os.getenv("WAFER_COLLECTION")].insert_many(json_record)
