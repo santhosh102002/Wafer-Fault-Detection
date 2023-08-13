533e1057cf625088fc83f79ef54e9470d42e785f
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-13 20:28:00 +0530

    app.py modified

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 
 COPY . /app
 
-
 RUN pip install -r requirements.txt
 
 CMD [ "python3","app.py" ]
--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def upload_data():
 
 if __name__ == "__main__":
 
-    app.run(debug=True,port=5000)
+    app.run()
